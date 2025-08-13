0a4c146d701201ae3d7c9ea7d5a99b10fb61e2c4
micoforreal <mjjames006@gmail.com>
2025-08-13 16:58:53 +0100

    33

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-plugins:[wasm() , topLevelAwait(),   tailwindcss(),],
+plugins:[wasm() , topLevelAwait(),   tailwindcss(),topLevelAwait()],
  build: {
         sourcemap: true,
 
-    //       rollupOptions: {
-    //   external: ['ammo.js'],
-    // }
+          rollupOptions: {
+      external: ['ammo.js'],
+    }
     },
   //   ssr: {
   // //   noExternal: ['ammo.js'] 
