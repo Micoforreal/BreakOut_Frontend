61e36efba9b61ae52866e22c233a9e9d094680c4
micoforreal <mjjames006@gmail.com>
2025-08-13 17:15:51 +0100

    33

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm';
-// import topLevelAwait from 'vite-plugin-top-level-await';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
 plugins:[wasm() , topLevelAwait(),   tailwindcss()],
